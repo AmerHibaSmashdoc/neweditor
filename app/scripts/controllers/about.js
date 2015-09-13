c0e5e3e0630f34b01d19a5fdde11dbebad2debb0
Amer Hiba <amer.hiba@smashdocs.net>
2015-09-13 21:40:24 +0200

    Add default angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name editorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the editorApp
+ */
+angular.module('editorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
